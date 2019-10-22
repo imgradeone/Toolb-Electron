cafbb73563df53c7f15b69b6b4166e4033b7d9bd
imgradeone <30969327+imgradeone@users.noreply.github.com>
2019-10-22 13:35:57 +0800

    Do it

--- README.md
+++ README.md
@@ -15,3 +15,28 @@ DAILY REVERTING
 ## 可以先尝试一下 Toolb 的 Android 版。
 
 <a href="https://github.com/imgradeone/ToolbReleases/releases/latest">![](https://img.shields.io/badge/download-forAndroid-0080ff.svg?style=for-the-badge)</a>
+
+# 加入项目开发
+
+由于 imgradeone 几乎没学过 JavaScript，所以这个项目几乎是白手起家，希望大佬给予帮助。
+
+参与开发很简单，
+
+## Fork 这个仓库，然后 clone 到本地
+
+接下来，
+
+## 安装依赖并开始开发
+
+```
+> yarn
+> yarn electron:serve
+> yarn electron:build
+```
+
+## 注：目前需要开发的功能
+
+- [ ] 基本 UI 设计，采用 Vuetify.js
+- [ ] JS 相关开发
+- [ ] Electron 无边框窗口的按钮事件
+- [ ] 创作中心 Lite
--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,61 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+    <v-system-bar window app style="-webkit-app-region: drag">
+      <v-icon>mdi-checkbox-blank-circle</v-icon>
+      <span>这里暂用作以后的标题栏</span>
+      <v-spacer></v-spacer>
+    </v-system-bar>
+    <v-app-bar app :elevate-on-scroll="true">
+      <v-toolbar-title class="headline">
+        <span>Toolb Electron</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    HelloWorld
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
+
+<style type="text/css" lang="scss">
+html {
+  overflow-y: hidden;
+  ::-webkit-scrollbar {
+    width: 5px; /*滚动条宽度*/
+    /*height: 2px;  !*滚动条高度*!*/
+  }
+
+  /*定义滚动条轨道 内阴影+圆角*/
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
+  }
+
+  /*定义滑块 内阴影+圆角*/
+  ::-webkit-scrollbar-thumb {
+    border-radius: 99px;
+    background-color: #cccccc;
+  }
+}
+
+.v-app-bar {
+  -webkit-user-select: none;
+}
+</style>
